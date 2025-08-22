79551b255a86393730ace2283b4a3de86b5de054
Sindre <sindresbrekke@gmail.com>
2025-08-22 20:05:34 +0200

    I GOT RUST TO WORK!!!

---
+++ guessing_game_rust/target/debug/deps/guessing_game_rust.d
@@ -0,0 +1,5 @@
+C:\Users\Sindre\OneDrive - Trøndelag fylkeskommune\Dokumenter\programering\rust\Guessing-game-rust\guessing_game_rust\target\debug\deps\guessing_game_rust.d: src\main.rs
+
+C:\Users\Sindre\OneDrive - Trøndelag fylkeskommune\Dokumenter\programering\rust\Guessing-game-rust\guessing_game_rust\target\debug\deps\guessing_game_rust.exe: src\main.rs
+
+src\main.rs:
